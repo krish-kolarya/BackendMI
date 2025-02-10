ee62137df2409dcf889d1a5e3321acacee079856
krish-kolarya <krishnakolarya@gmail.com>
2025-02-10 22:21:25 +0530

    Database connection

---
+++ src/db/index.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+export const DBConnection = async () => {
+    try {
+        const connectionInstance = await mongoose.connect (`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`DB connected! Host: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("Database Connection Error: ", error);
+        process.exit(1);
+    }
+}
